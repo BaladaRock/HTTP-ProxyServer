05e964d2a686be9495ccf3962e2adfdeaf23a389
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2020-04-29 17:13:36 +0300

    GetPositions has been optimized

--- proxy_server/HttpParser.cs
+++ proxy_server/HttpParser.cs
@@ -20,11 +20,7 @@ namespace ProxyHTTP
 
         public int GetPosition(byte[] array, byte[] subArray)
         {
-            int position = SubArrayIndex(array, subArray);
-
-            return position == -1
-                ? position
-                : position + subArray.Length;
+            return SubArrayPosition(array, subArray);
         }
 
         public (int first, int second) GetPositions(
@@ -32,35 +28,22 @@ namespace ProxyHTTP
              byte[] firstArray,
              byte[] secondArray)
         {
-            (int , int) indexes = (-1, -1);
-            int firstCount = 0, secondCount = 0;
+            (int first, int second) indexes = SubArraysPositions(
+                readLine,
+                firstArray,
+                secondArray,
+                out int count);
 
-            for (int index = 0; index < readLine.Length; index++)
+            if (indexes.first != -1)
             {
-                if (readLine[index] != firstArray[firstCount++])
-                {
-                    firstCount = 0;
-                }
-
-                if (readLine[index] != secondArray[secondCount++])
-                {
-                    secondCount = 0;
-                }
-
-                if (firstCount == firstArray.Length)
-                {
-                    indexes.Item1 = index - firstCount + 1;
-                }
-
-                if (secondCount == secondArray.Length)
-                {
-                    indexes.Item2 = index - secondCount + 1;
-                }
+                indexes.second = SubArrayPosition(readLine, secondArray, indexes.first, count);
+            }
+            else if (indexes.second != -1)
+            {
+                indexes.first = SubArrayPosition(readLine, firstArray, indexes.second, count);
             }
 
             return indexes;
-
-            //return (GetPosition(readLine, firstArray), GetPosition(readLine, secondArray));
         }
 
         public bool IsChunkComplete(byte[] byteLine, string ending, int minimumSize = 0)
@@ -91,11 +74,11 @@ namespace ProxyHTTP
             return remainingBytes;
         }
 
-        private int SubArrayIndex(byte[] array, byte[] subArray)
+        private int SubArrayPosition(byte[] array, byte[] subArray, int start = 0, int countIndex = 0)
         {
-            int count = 0;
+            int count = countIndex;
 
-            for (int index = 0; index < array.Length; index++)
+            for (int index = start; index < array.Length; index++)
             {
                 if (array[index] != subArray[count++])
                 {
@@ -104,11 +87,49 @@ namespace ProxyHTTP
 
                 if (count == subArray.Length)
                 {
-                    return index - count + 1;
+                    return index + 1;
                 }
             }
 
             return -1;
+        }
+
+        private (int first, int second) SubArraysPositions(
+             byte[] array,
+             byte[] firstArray,
+             byte[] secondArray,
+             out int countStart,
+             int positionStart = 0)
+        {
+            (int, int) count = default;
+
+            for (int index = positionStart; index < array.Length; index++)
+            {
+                if (array[index] != firstArray[count.Item1++])
+                {
+                    count.Item1 = 0;
+                }
+
+                if (array[index] != secondArray[count.Item2++])
+                {
+                    count.Item2 = 0;
+                }
+
+                if (count.Item1 == firstArray.Length)
+                {
+                    countStart = count.Item2;
+                    return (index + 1, -1);
+                }
+
+                if (count.Item2 == secondArray.Length)
+                {
+                    countStart = count.Item1;
+                    return (-1, index + 1);
+                }
+            }
+
+            countStart = 0;
+            return (-1, -1);
         }
     }
 }
