704ec9827ecd44c74064e0987af4d60e75ab76f4
BaladaRock <48443239+BaladaRock@users.noreply.github.com>
2020-08-22 13:14:08 +0300

    Started implementation for HTTPS(SSL tunnel). Added RequestReader.cs(for parsing requests from browser)

---
+++ ProxyHTTP_Facts/RequestReaderFacts.cs
@@ -0,0 +1,24 @@
+﻿using ProxyHTTP;
+using ProxyServer;
+using Xunit;
+
+namespace ProxyHTTP_Facts
+{
+    public class RequestReaderFacts
+    {
+        [Fact]
+        public void Connect_Should_be_TRUE_for_SIMPLE_Request()
+        {
+            //Given
+            const string request = "CONNECT www.google-analytics.com:443 HTTP/1.1";
+            var reader = new RequestReader(request);
+            //When
+            reader.CheckRequest();
+            //Then
+            Assert.True(reader.Connect);
+        }
+
+
+
+    }
+}
---
+++ proxy_server/RequestReader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProxyHTTP
+{
+    public class RequestReader
+    {
+        private string request;
+
+        public RequestReader(string request)
+        {
+            this.request = request;
+        }
+
+        public bool Connect { get; internal set; }
+
+        internal void CheckRequest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
